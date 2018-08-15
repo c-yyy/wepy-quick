3d0bc9bb0b109bbbd9f922147782bb8eabd6f52a
inpm <inpm.cy@gmail.com>
2018-08-15 21:30:16 +0800

    去掉冗余

--- dist/_mock_/_mock_.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIl9tb2NrXy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwiZmlsZSI6Il9tb2NrXy5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
--- dist/npm/lodash/_metaMap.js
+++
@@ -1,6 +0,0 @@
-var WeakMap = require('./_WeakMap.js');
-
-/** Used to store function metadata. */
-var metaMap = WeakMap && new WeakMap;
-
-module.exports = metaMap;
